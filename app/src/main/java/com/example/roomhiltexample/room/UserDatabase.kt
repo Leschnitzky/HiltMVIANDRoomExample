163356e002dfc7dea67abef4dc768a7c1cfa9cac
Sergey Leschinsky <leschinskysergey@gmail.com>
2021-03-29 18:00:44 +0300

    Working adding user and displaying them on screen

--- app/src/main/java/com/example/roomhiltexample/room/UserDatabase.kt
+++ app/src/main/java/com/example/roomhiltexample/room/UserDatabase.kt
@@ -3,7 +3,7 @@ package com.example.roomhiltexample.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [UserCacheEntity::class], version = 1)
+@Database(entities = [UserCacheEntity::class], version = 2)
 abstract class UserDatabase : RoomDatabase(){
     abstract fun userDao(): UsersDao
 
